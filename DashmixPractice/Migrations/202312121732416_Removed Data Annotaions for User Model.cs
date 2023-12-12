4ecac014602122c2815570e88e1f75907ae49edb
DixitBhavyang <dixitbhavyang111@gmail.com>
2023-12-12 23:18:46 +0530

    Removed Data Annotations form User

---
+++ DashmixPractice/Migrations/202312121732416_Removed Data Annotaions for User Model.cs
@@ -0,0 +1,24 @@
+﻿namespace DashmixPractice.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RemovedDataAnnotaionsforUserModel : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Users", "Name", c => c.String());
+            AlterColumn("dbo.Users", "Username", c => c.String());
+            AlterColumn("dbo.Users", "Email", c => c.String());
+            AlterColumn("dbo.Users", "Password", c => c.String(maxLength: 8));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Users", "Password", c => c.String(nullable: false, maxLength: 8));
+            AlterColumn("dbo.Users", "Email", c => c.String(nullable: false));
+            AlterColumn("dbo.Users", "Username", c => c.String(nullable: false));
+            AlterColumn("dbo.Users", "Name", c => c.String(nullable: false));
+        }
+    }
+}
--- DashmixPractice/Models/User.cs
+++ DashmixPractice/Models/User.cs
@@ -12,23 +12,17 @@ namespace DashmixPractice.Models
         [Key]
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "* Name is Required. . . ")]
         public string Name { get; set; }
 
         public string ProfilePicturePath { get; set; }
 
-        [Required(ErrorMessage = "* Username is Required. . . ")]
         public string Username { get; set; }
 
-        [Required(ErrorMessage = "* Email is Required . . .")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "* Password is Required . . .")]
         [StringLength(8)]
         public string Password { get; set; }
 
-        [Required(ErrorMessage = "* Confirm Password is Required . . .")]
-        [Compare("Password", ErrorMessage = "The Password and Confirmation password do not match.")]
         [NotMapped]
         public string ConfirmPassword { get; set; }
 
