3cfd30990fd5050cf1b6a37010d18df4aa914e76
DixitBhavyang <dixitbhavyang111@gmail.com>
2023-12-12 23:19:08 +0530

    Created ViewModel for User and ContactInfo

---
+++ DashmixPractice/ViewModels/UserContactInformationViewModel.cs
@@ -0,0 +1,14 @@
+﻿using DashmixPractice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DashmixPractice.ViewModels
+{
+    public class UserContactInformationViewModel
+    {
+        public User User { get; set; }
+        public Contact_Information ContactInforamation { get; set; }
+    }
+}
